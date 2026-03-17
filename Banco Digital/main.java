b437cdc175fa42b15335f3a7a9d329923366f881
GabrielDuarte-collab <joaogabr44@gmail.com>
2026-03-17 01:00:03 +0000

    salva

---
+++ Banco Digital/main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int = opcao;    
+    
+        do{
+        telas.menuPrincipal();
+        opcao = Integer.parseInt(sc.nextLine().()trim);
+
+         switch (opcao) {
+            case 1:
+                telas.mensagem("opçao 1 selecionada : criar conta (criar em breve)");
+                break;
+            case 2:
+                telas.mensagem("opçao 2 selecionada : Acessar conta");
+                break;
+            case 3:
+                telas.mensagem("opçao 3 selecionada :")
+                break;
+            case 4:
+                telas.mensagem("")
+                break;
+            }
+        }
+   
+        
+    }
+}
---
+++ Banco Digital/telas.java
@@ -0,0 +1,14 @@
+public class telas {
+    
+
+public static menuPrincipal() {
+    System.out.println("\n=== MENU ===");
+    System.out.println("1 - Criar conta");
+    System.out.println("2 - Acessar conta");
+   
+}
+static menuConta() {
+
+}
+ 
+}
